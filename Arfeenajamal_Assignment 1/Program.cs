abfaa4c716ae9ca3c81ea95f9fe005dab60eeae2
shahi <shahi@HP>
2024-05-30 03:30:53 +0530

    Add project files.

---
+++ Arfeenajamal_Assignment 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class My_pet
+{
+    static void Main()
+    {
+        Console.WriteLine("choose pets from the given list:");
+        Console.WriteLine("1:RABBIT");
+        Console.WriteLine("2:CAT");
+        Console.WriteLine("3:PARROT");
+        string pet = Console.ReadLine();
+        Console.WriteLine("ENTER YOUR PET NAME");
+        Console.WriteLine("welcome to the pet simulator");
+    }
+}
